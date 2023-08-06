45001d18aa5697cc8b7a686c102368f7156a44e5
RuduBear <134881527+rudubear@users.noreply.github.com>
2023-08-06 02:15:23 -0400

    grid created with mouse trails

--- js/javascript.js
+++ js/javascript.js
@@ -1,2 +1,24 @@
 const etchASketchPad = document.getElementById("etchASketch")
+const gridSideLength = 12;
+
+etchASketchPad.addEventListener("click",markSquare)
+
 console.log(etchASketchPad);
+
+populateEtchASketch();
+
+function populateEtchASketch(){
+    for(let x = 0; x< gridSideLength**2 ; x++){
+        const square = document.createElement('div');
+        square.classList += `square`;
+        const squareSideLengthFlexBasis = Math.round((100/gridSideLength) * 100) / 100;
+        //console.log(newFlexBasis);
+        square.style.flexBasis = `${squareSideLengthFlexBasis}%`;
+        etchASketchPad.appendChild(square);
+    }
+}
+
+function markSquare(e) {
+    console.log(e)
+}
+
