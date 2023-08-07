2a882eddc4351a40ff694986f82d2991199a53c1
RuduBear <134881527+rudubear@users.noreply.github.com>
2023-08-07 00:17:24 -0400

    fixed an issue with mouse trail missing, hover state was overwritten

--- js/javascript.js
+++ js/javascript.js
@@ -3,14 +3,17 @@ const DEFAULT_MARKER_COLOR = "white"
 const DEFAULT_SIDE_PANEL_COLOR = "brown";
 const DEFAULT_SQUARE_HOVER_COLOR = "darkorange";
 const DEFAULT_PAGE_BG_COLOR = "blanchedalmond";
-const DEFAULT_BODY_COLOR = "burlywood";
+const DEFAULT_ETCHASKETCH_COLOR = "burlywood";
 const DEFAULT_GRID_SIDE_LENGTH = 16;
 
+let isRandomMarkerColor = false ;
+let mouseButtonIsPressed = undefined;
 
 const etchASketchPad = document.getElementById("etchASketch")
 const dontTriggerDefault = (e) => {e.preventDefault()};
+const toggleRandomColorsButton = document.getElementById("btn_toggleRandomColors");
+const selectedColor = document.getElementById("selectedColor");
 
-let mouseButtonIsPressed = undefined;
 
 
 etchASketchPad.addEventListener("mousemove", captureMouseButtonState);
@@ -26,6 +29,7 @@ function populateEtchASketch(gridSideLength){
         const square = document.createElement('div');
         square.classList += `square`;
         
+        //square.style.backgroundColor = DEFAULT_ETCHASKETCH_COLOR;
         //adding an XY coordinate mapping via attributes to the divs. No use for these right now but might use them later.
         square.setAttribute("data-coordX",`${x % gridSideLength}`);
         square.setAttribute("data-coordY",`${Math.floor(x/gridSideLength)}`);
@@ -51,18 +55,50 @@ function captureMouseButtonState(e){
     }
     //console.log(mouseButtonIsPressed);
     if(mouseButtonIsPressed) {
-        markSquares(e, DEFAULT_MARKER_COLOR);
+        markSquares(e, isRandomMarkerColor ? getRandomColor() : DEFAULT_MARKER_COLOR);
     }
 }
 
 function markSquares(e, MARKER_COLOR) {
     const selectedSquare = e.srcElement;
     selectedSquare.style.backgroundColor = MARKER_COLOR;
+    getRandomColor();
+}
+
+function getRandomColor(){
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
+    console.log(`R = ${red} G = ${green} B = ${blue}`);
+    return `rgb(${red},${green},${blue})`;
+}
+
+function toggleRandomColors(){
+    if(isRandomMarkerColor) {
+        isRandomMarkerColor = false;
+        toggleRandomColorsButton.textContent = "Single Color";
+    }
+    else {
+        isRandomMarkerColor = true;
+        toggleRandomColorsButton.textContent = "Random Color";
+    }
+
+    console.log(selectedColor);
+}
+
+function setMarkerColor(){
+//TODO:
+}
+
+function resetEtchASketch(){
+    populateEtchASketch(DEFAULT_GRID_SIDE_LENGTH);
+    isRandomMarkerColor = false;
+    toggleRandomColorsButton.textContent = "Random Color";
 }
 
 function setGridSize(){
     let newGridSize = prompt("Please Enter a new Grid Length (1-100");
-    if (Number.isInteger(Number(newGridSize))) {
+    if (Number.isInteger(Number(newGridSize)) && newGridSize !== "" && newGridSize !== null) {
         etchASketchPad.textContent = "";
         populateEtchASketch(newGridSize);
     }
